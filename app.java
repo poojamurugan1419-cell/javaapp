0e0c6d31478b2738cda10d4daac7e8cf95773c41
root <root@ip-172-31-31-195.ec2.internal>
2026-07-21 10:23:49 +0000

    Initial commit - Added App.java

---
+++ app.java
@@ -0,0 +1,24 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" Welcome to Jenkins CI/CD Demo ");
+        System.out.println("================================");
+
+        String appName = "Employee Management System";
+        String version = "1.0.0";
+
+        System.out.println("Application : " + appName);
+        System.out.println("Version     : " + version);
+        System.out.println("Status      : Running Successfully");
+
+        System.out.println("\nFeatures:");
+        System.out.println("- Employee Registration");
+        System.out.println("- Employee Search");
+        System.out.println("- Employee Update");
+        System.out.println("- Employee Delete");
+
+        System.out.println("\nBuild completed successfully!");
+    }
+}
